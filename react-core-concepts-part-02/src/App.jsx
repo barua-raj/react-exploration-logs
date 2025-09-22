389a7c538fce0a3d3065fff8d2a8febef26844b6
barua-raj <barua.jovraj@gmail.com>
2025-09-23 01:20:43 +0600

    react use

--- react-core-concepts-part-02/src/App.jsx
+++ react-core-concepts-part-02/src/App.jsx
@@ -1,10 +1,22 @@
+import { Suspense } from 'react';
 import './App.css'
 // import Batsman from './Batsman';
 // import Counter from './Counter';
 import Runs from './Runs'
+import Users from './Users';
+import Friends from './Friends';
+
+const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
+.then((res) => res.json())
+
+const fetchFriends = async() => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  return res.json();
+};
 
 function App() {
 
+  const friendsPromise = fetchFriends();
 
   const handleClick = () => {
     alert('I am clicked!');
@@ -18,8 +30,20 @@ function App() {
   return (
     <>
       
-      {/* <h2>React</h2> */}
+      <h2>Vite + React</h2>
+
+      <Suspense fallback = {<h3>Loading...</h3>}>
+        <Users fetchUsers = {fetchUsers}></Users>
+      </Suspense>
       
+      <Suspense fallback = {<h3>Friends are coming...</h3>}>
+        <Friends friendsPromise = {friendsPromise}></Friends>
+      </Suspense>
+
+
+      {/* <Users></Users> */}
+
+
       <Runs></Runs>
 
       {/* <Batsman></Batsman> */}
